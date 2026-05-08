77737bdc74e64d4cdd8580d7d3743e605e96590e
WaseelArar <WaseelArar@gmail.com>
2026-05-08 18:07:00 +0300

    fixed users table on small screen

--- src/routes/admin/users/+page.svelte
+++ src/routes/admin/users/+page.svelte
@@ -157,7 +157,7 @@
     <div
         class="bg-white border border-slate-200 rounded-3xl overflow-hidden shadow-sm"
     >
-        <table class="w-full text-right border-collapse {!canManageUsers ? 'hide-actions' : ''}">
+        <table class="w-full overflow-y-scroll text-right border-collapse {!canManageUsers ? 'hide-actions' : ''}">
             <thead class="bg-slate-50 border-b border-slate-100">
                 <tr>
                     <th class="p-4 font-bold text-slate-600">المستخدم</th>
